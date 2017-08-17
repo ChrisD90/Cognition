f59ced1750338b45e48515da259fc554c5717011
Chris D <christoph.doeringer+github@gmail.com>
2017-08-17 21:22:42 +0200

    Layout changes

--- app/src/main/java/com/example/chris/cognition/Liga3Activity.java
+++ app/src/main/java/com/example/chris/cognition/Liga3Activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -12,23 +13,28 @@ public class Liga3Activity extends AppCompatActivity {
 
     int layout_width;
     int layout_height;
+    RelativeLayout layout;
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liga3);
+        layout = (RelativeLayout) findViewById(R.id.action_screen);
+        image = (ImageView) findViewById(R.id.star);
         getLayoutSize();
         setImage();
     }
 
     public void getLayoutSize(){
-
-       RelativeLayout layout = (RelativeLayout) findViewById(R.id.action_screen);
-
+       float x = image.getX();
+        float y = image.getY();
        layout_width = layout.getWidth();
        layout_height = layout.getHeight();
+
             }
 
     public void setImage() {
+
 
     }
 }

19798b628c7b55bb5ce29a6e30430a6834c5aa7b
Chris D <christoph.doeringer+github@gmail.com>
2017-08-17 20:52:05 +0200

    Layout changes

--- app/src/main/java/com/example/chris/cognition/Liga3Activity.java
+++ app/src/main/java/com/example/chris/cognition/Liga3Activity.java
@@ -1,13 +1,34 @@
 package com.example.chris.cognition;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class Liga3Activity extends AppCompatActivity {
 
+    int layout_width;
+    int layout_height;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liga3);
+        getLayoutSize();
+        setImage();
+    }
+
+    public void getLayoutSize(){
+
+       RelativeLayout layout = (RelativeLayout) findViewById(R.id.action_screen);
+
+       layout_width = layout.getWidth();
+       layout_height = layout.getHeight();
+            }
+
+    public void setImage() {
+
     }
 }
